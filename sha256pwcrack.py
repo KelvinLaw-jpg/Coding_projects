23dfa757bac60364bac6e973e4c20425d7290398
Kelvin.tc.Law <73468336+KelvinLaw-jpg@users.noreply.github.com>
2025-04-24 17:10:00 +0100

    Update sha256pwcrack.py

--- sha256pwcrack.py
+++ sha256pwcrack.py
@@ -1,16 +1,18 @@
 from pwn import *
 import sys
 
-
+#check if there is input
 if len(sys.argv) != 3:
 	print("Invalid arguments!")
 	print(">> {} <sha256sum> <list_to_crack>".format(sys.argv[0]))
 	exit()
 
+#variables needed
 wanted_hash = sys.argv[1]
 password_file = sys.argv[2]
 attempts = 0
 
+#log lib.progress is a handy function to return a log object to track for progress, and return result
 with log.progress("Attempting to crack: {}!\n".format(wanted_hash)) as p:
 	with open(password_file, "r", encoding='latin-1') as password_list:
 		for password in password_list:
